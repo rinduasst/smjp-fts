669ebada7ec9fee800edaad3b09820aee8c8a2f7
Rindu Astuti <rinduasst@gmail.com>
2026-04-19 11:47:39 +0700

    update env

--- src/api/api.js
+++ src/api/api.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-  baseURL: "http://43.129.55.147:3000/api-backend-smjp"
- 
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 // otomatis kirim token
